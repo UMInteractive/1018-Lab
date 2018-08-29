3720537f52b2c88da0d1c393a76763c58e5c2a2d
Zevensuy Rodriguez <zevenrodriguez@gmail.com>
2018-08-29 12:24:45 -0400

    home

--- js/views.js
+++ js/views.js
@@ -13,7 +13,7 @@ window.onload = function () {
     var navLinks = new Vue({
     el: '#navLinks',
     data: {
-      menu: '<ul><li><a href="./../3DPrinting">3D Printing</a></li><li><a href="./../PaperCutter">Paper Cutter</a></li></ul>'
+      menu: '<ul><li><a href="./">Home</a></li><li><a href="./../3DPrinting">3D Printing</a></li><li><a href="./../PaperCutter">Paper Cutter</a></li></ul>'
     }
   });
 
